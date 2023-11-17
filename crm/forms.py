858cf0e2aa069f81e24757f41d87cbc993afac85
deepak <deepakdeepz1230@gmail.com>
2023-11-17 22:01:38 +0530

    new one

--- crm/forms.py
+++ crm/forms.py
@@ -28,7 +28,7 @@ class EmployeeModelForm(forms.ModelForm):
             "email":forms.EmailInput(attrs={"class":"form-control"}),
             "age":forms.NumberInput(attrs={"class":"form-control"}),
             "contact":forms.Textarea(attrs={"class":"form-control","rows":5}),
-            "dob":forms.DateField(attrs={"class":"form-control","type":"date"})
+            "dob":forms.DateInput(attrs={"class":"form-control","type":"date"})
 
 
 
